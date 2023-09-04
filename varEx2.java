6febdfa33f5b09a7e0aaa471a6579dcff7e5f6d8
Shreyash862385 <144019679+Shreyash862385@users.noreply.github.com>
2023-09-04 17:09:57 +0530

    Create varEx2.java
    new file added

---
+++ varEx2.java
@@ -0,0 +1,19 @@
+class varEx2
+{
+	public static void main(String[] args)
+	{
+		int number = 23;
+		
+		if(number % 2 == 0)
+		{
+			System.out.println(number +" is even number");
+		}
+		else
+		{
+			System.out.println(number +" is odd number");
+		}
+			
+		
+		
+	}
+}	
