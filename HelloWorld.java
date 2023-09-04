cbfdb4cb8c105bb35bf7ae134b306617f44a87ed
Shreyash862385 <144019679+Shreyash862385@users.noreply.github.com>
2023-09-04 16:54:29 +0530

    Create HelloWorld.java
    One file added

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		int rollno;
+		rollno = 10;
+		
+		int age = 21;
+		double price = 105.55;
+		boolean isFree = true;
+		
+		System.out.println(age);
+		System.out.println(price);
+		System.out.println(isFree);
+	}
+}	
